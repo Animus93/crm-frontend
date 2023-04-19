7db73ec0c714d8fda6188a934f854c813f24a262
Dmitiryi Eremin <unitofpeople@gmail.com>
2023-04-19 14:04:35 +0300

    first commit

--- src/pages/Main/TotalContacts/TotalContacts.jsx
+++ src/pages/Main/TotalContacts/TotalContacts.jsx
@@ -1,16 +1,15 @@
 import ButtonAdd from "../../../components/MainRight/ButtonAdd/ButtonAdd";
 import Table from "../../../components/MainRight/Table/Table";
-import styles from './totalContacts.module.css'
+import styles from "./totalContacts.module.css";
+
 function TotalContacts() {
   return (
     <div className={styles.wrapper}>
-        <div className={styles.title}>Total Contacts
-        </div>
-        <div className={styles.outlet}>
-        <Table/>
-        </div>
-        <ButtonAdd/>
-        <div className={styles.pagenation}>1,2,3,4</div>
+      <div className={styles.title}>Total Contacts</div>
+      <div className={styles.outlet}>
+        <Table />
+      </div>
+      <ButtonAdd />
     </div>
   );
 }
